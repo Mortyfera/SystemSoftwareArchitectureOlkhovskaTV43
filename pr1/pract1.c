8c3012245979fa1593a521b6894b74dd16a51713
morty <liza8203788@gmail.com>
2026-02-11 11:16:17 +0200

    added new function and changed code a little

--- pr1/pract1.c
+++ pr1/pract1.c
@@ -1,72 +1,98 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int * change_array_size(int *arr){
-    int new_number;
+#define MIN_SIZE 1
+#define MAX_SIZE 1000
 
-    printf("Enter size to which you want to change the size of your existing array: ");
-    scanf("%d", &new_number);
-
-    int *temp = (int*)realloc(arr, new_number * sizeof(int));
-
-    if(temp == NULL){
-        printf("\nError. Were not able to change the size of array.");
-        return arr;
-    }else{
-        printf("\nThe size of array was changed succesfully.");
-        for (int i=0; i<new_number; i++){
-            temp[i] = new_number;
-        }
-        printf("\nArray: ");
-        for (int i=0; i<new_number; i++){
-        printf("%d ", temp[i]);
+void print_memory_status(const char* operation, void* ptr, int size) {
+    if (ptr != NULL) {
+        printf("[LOG] %s is success. Address: %p, Current Size: %d elements (%lu bytes)\n", operation, ptr, size, size * sizeof(int));
+    } else {
+        printf("[LOG] %s has failed or memory was freed.\n", operation);
     }
+}
+
+int* change_array_size(int *arr, int *current_size) {
+    int new_size;
+
+    printf("\nEnter new size for the array (%d - %d): ", MIN_SIZE, MAX_SIZE);
+    scanf("%d", &new_size);
+
+    if (new_size < MIN_SIZE || new_size > MAX_SIZE) {
+        printf("Error. Size must be between %d and %d.\n", MIN_SIZE, MAX_SIZE);
+        return arr;
+    }
+
+    int *temp = (int*)realloc(arr, new_size * sizeof(int));
+
+    if (temp == NULL) {
+        printf("\nError. Unable to reallocate memory.\n");
+        return arr;
+    } else {
+        *current_size = new_size;
+        
+        print_memory_status("Realloc", temp, new_size);
+        
+        for (int i = 0; i < new_size; i++) {
+            temp[i] = new_size;
+        }
+
+        printf("Array resized and refilled:\n");
+        for (int i = 0; i < new_size; i++) {
+            printf("%d ", temp[i]);
+        }
+        printf("\n");
+        
         return temp;
     }
 }
 
-int main(){
-    int number;
+int main() {
+    int size;
     int *arr = NULL;
     
-    printf("Enter size of the dinamic array you want to create: ");
-    scanf("%d", &number);
+    printf("Enter initial size of the dynamic array (%d - %d): ", MIN_SIZE, MAX_SIZE);
+    scanf("%d", &size);
+
+    if (size < MIN_SIZE || size > MAX_SIZE) {
+        printf("Error. Invalid size.\n");
+        return 1;
+    }
     
-    arr = (int*)malloc(number * sizeof(int));
+    arr = (int*)malloc(size * sizeof(int));
     
-    if(arr == NULL){
-        printf("\nError. Space was't created.");
+    if (arr == NULL) {
+        printf("\nError. Memory not allocated.\n");
         return 1;
     }
 
-    for (int i=0; i<number; i++){
-        arr[i] = number;
+    print_memory_status("Malloc", arr, size);
+
+    for (int i = 0; i < size; i++) {
+        arr[i] = size;
     }
 
-    printf("\nArray: ");
-    for (int i=0; i<number; i++){
+    printf("Initial Array: ");
+    for (int i = 0; i < size; i++) {
         printf("%d ", arr[i]);
     }
-
     printf("\n");
 
     char answer;
+    
+    printf("\nDo you want to change the size of the existing array (y/n): ");
+    scanf(" %c", &answer); 
 
-    printf("Do you want to change the size of the existing array(y/n): ");
-    scanf("%c", &answer);
-
-    while(answer == 'y' || answer == 'Y'){
-         if(answer == 'y' || answer == 'Y'){
-            arr = change_array_size(arr);
-        }else if(answer == 'n' || answer == 'N'){
-            free(arr);
-            arr = NULL;
-            return 0;
-        }else{
-            printf("Error. Do you want to change the size of the existing array(y/n): ");
-            scanf("%c", &answer);
-        }
-        printf("Do you want to change the size of the existing array(y/n): ");
-        scanf("%c", &answer);
+    while (answer == 'y' || answer == 'Y') {
+        arr = change_array_size(arr, &size);
+        
+        printf("\nDo you want to change the size again (y/n): ");
+        scanf(" %c", &answer);
     }
+
+    free(arr);
+    arr = NULL;
+    print_memory_status("Free", arr, 0);
+
+    return 0;
 }
